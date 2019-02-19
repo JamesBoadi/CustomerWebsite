baed044cdb4d21d3d1f8237f931194f9874aa50b
JamesBoadi <39907481+JamesBoadi@users.noreply.github.com>
2019-02-19 22:30:29 +0000

    Registration and sign up

--- loginregister.php
+++ loginregister.php
@@ -1,14 +1,70 @@
 <?php
 
-  //Reference to a functions.php file
-  include 'functions.php';
+//Reference to a functions.php file
+include 'functions.php';
 
-  //Header and navigation bar functions from functions.php file
-  outputHeader("Tanks");
-  navBar("Login / Sign Up");
+
+//Header and navigation bar functions from functions.php file
+outputHeader("Tanks");
+navBar("Login / Sign Up");
+$manager = new MongoClient(); // Set up mongo db client
+
+$db = $manager->selectDb("registration"); // select a database
+$collection = $db->selectCollection("mycol"); // select a collection
+
+$username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
+$password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
+if (!empty($_POST)) {
+
+  if (isset($_POST["password"]) && isset($_POST["username"])) { 
+    // Validation could be added here
+    $registerArray = ["username" => $username, "password" => $password]; // Json format
+    if(!empty($username) && !empty($password)) // Check if it is empty
+    {
+      $collection->insert($registerArray); // Insert
+    }
+    else
+    {
+      echo "<script>alert('All fields must be filled in');</script>";
+    }
+    
+  } else {
+    echo "<script>console.log('Both the username and password must be entered');</script>";
+  }
+  
+  //Extract the data that was sent to the server
+  if (isset($_POST["password_"]) ) {   // Get the data from log in
+    $getUsername = filter_input(INPUT_GET, 'username_', FILTER_SANITIZE_STRING);
+    $getPassword = filter_input(INPUT_GET, 'password_', FILTER_SANITIZE_STRING);
+    
+    //Create a PHP array with our search criteria
+    $findCriteria = [
+      $getUsername
+    ];
+  
+    //Find all of the customers that match  this criteria
+    $cursor = $db->selectCollection("mycol")->find($findCriteria);
+  
+      //Output the results
+      foreach ($cursor as $cust) { // Check if password exists or not
+          if($getPassword != $cust['password'] && empty($getPassword) )
+          {
+            echo "<script>alert('Password is incorrect');</script>";
+          }
+      }
+    }
+}
+
+  
+
+
+
+$manager->close();
+// ----------------------------------------------------------------------https://stackoverflow.com/questions/24985684/mongodb-show-all-contents-from-all-collections
+
+// Get data from database
 
 ?>
-
     <!-- Login and Register container -->
     <div id="LogRegContainer">
 
@@ -19,24 +75,28 @@
       </ul>
 
       <!-- Content and input fields of the Login tab -->
+      <form action="loginregister.php" method="POST"> <!-- Handle the LOGIN POST REQUEST HERE -->
       <div id="log" class="inputfield">
         <a>Username</a>
-        <input id="usernameL" type="text" name="user">
+        <input id="usernameL" type="text" name="username_">
         <a>Password</a>
-        <input id="passwordL" type="password">
-        <button id="logBtn" type="button" onclick="logIn(users)">Login</button>
+        <input id="passwordL" type="password" name="password_">
+        <input id="logInButton" type="submit" onclick="logIn(users)" value="Log in"> <!-- Changes made to allow post request--> 
       </div>
+      </form>
 
       <!-- Content and input fields of the Register tab -->
+      <form action="loginregister.php" method="POST">
       <div id="reg" class="inputfield">
         <a>Username</a>
-        <input id="usernameR" type="text" name="user">
+        <input id="usernameR" type="text" name="username">
         <a>Password</a>
-        <input id="passwordR" type="password">
+        <input id="passwordR" type="password" name="password">
         <a>Repeat Password</a>
-        <input id="passwordRrepeated" type="password">
-        <button id="regBtn" type="button" onclick="signUp(users)">Sign Up</button>
+        <input id="passwordRrepeated" type="password" name="confirm-password">
+        <input id="registerButton" type="submit" onclick="signUp(users)" value="Sign up">
       </div>
+      </form>
 
       <div id="logedIn" class="inputfield">
         <a id="LoggedInAs"></a>
@@ -46,15 +106,11 @@
 
       <!--Script for login and sign up functionality -->
       <script src="javascript/loginSignUp.js"></script>
-
     </div>
-
-
 
 
 <?php
 
-  //Footer function from functions.php file
-  footer();
+footer();
 
 ?>
